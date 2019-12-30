89fb8a1ba3b7032c54b533e2fb2a94021137871d
Saim <saimh355@gmail.com>
2019-12-30 22:48:09 +0500

    Simple chat application with Node.js

--- app/src/main/java/com/example/nodechatapp/MainActivity.java
+++ app/src/main/java/com/example/nodechatapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.nodechatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtText = findViewById(R.id.edt_text);
+        findViewById(R.id.enterButton)
+                .setOnClickListener(v -> {
+                    Intent intent = new Intent(this,ChatActivity.class);
+                    intent.putExtra("name",edtText.getText().toString());
+                    startActivity(intent);
+                });
+
     }
 }
